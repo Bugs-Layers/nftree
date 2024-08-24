7ded06acd4bf20b6e8230a54d60d769d20da0531
Neha Sinha <nehasinha123987@gmail.com>
2024-08-25 03:01:39 +0530

    jmkhchmb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Onboarding from "~/components/Onboarding"
 import Profile from "~/components/Profile"
 
 function page() {
   return (
     <div>
       <Profile avatar={"placeholder-user.jpg"} username={"John Doe"} description={"I am a software engineer who loves building cool stuff with code."} trees={100} carboncredits={290} />
-      {/* <Onboarding walletAddress={"htf hfhfr"}/> */}
+      <Onboarding walletAddress={"htf hfhfr"}/>
     </div>
   )
 }
