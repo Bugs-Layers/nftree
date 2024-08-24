7d700d6be3451fe07964b741b7dc10ab8a128759
shahank42 <shounakghosh42@gmail.com>
2024-08-25 03:06:07 +0530

    chore(app): idek

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -1,10 +1,28 @@
 "use client";
 
 import { ConnectButton } from "thirdweb/react";
-import { client } from "~/lib/thirdWebClient";
+import { generatePayload, isLoggedIn, login, logout, setWalletAddressCookie } from "~/lib/thirdweb/actions";
+import { client } from "~/lib/thirdweb/client";
 
 export default function LoginButton() {
   return (
-    <ConnectButton client={client} />
+    <ConnectButton
+      client={client}
+
+      auth={{
+        isLoggedIn: async (address) => {
+          await setWalletAddressCookie(address)
+          return await isLoggedIn()
+        },
+        doLogin: async (params) => {
+          await login(params);
+        },
+        getLoginPayload: async ({ address }) =>
+          generatePayload({ address }),
+        doLogout: async () => {
+          await logout();
+        },
+      }}
+    />
   )
 }
