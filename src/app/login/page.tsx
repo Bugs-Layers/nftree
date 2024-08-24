648ac76e5d5cd10f73a057db36aeaa3b13c81df5
shahank42 <shounakghosh42@gmail.com>
2024-08-25 02:16:23 +0530

    chore(app): added thirdweb provider

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,18 @@
 import { Button } from "~/components/ui/button";
-import Link from "next/link";
 import Image from 'next/image';
+import LoginButton from "~/components/login-button";
 export default function page() {
   return (
     <div className="overflow-y-hidden h-screen">
-    <div className="flex justify-center items-center m-0 absolute top-[50%] translate-y-[-50%] translate-x-[237%] h-screen">
-      <Button asChild className="" >
-        <Link href="/login">Login</Link>
-      </Button>
-    </div>
-    <div className="flex justify-center items-center top-[100%] translate-y-[150%] dark:hidden ">
-        <Image src="/login_image.png" alt="login" width={500} height={500}  />
-    </div>
-    <div className="flex justify-center items-center top-[100%] translate-y-[150%]">
-        <Image src="/login_image_white.png" alt="login" width={500} height={500}  />
-    </div>
+      <div className="flex justify-center items-center m-0 absolute top-[50%] translate-y-[-50%] translate-x-[237%] h-screen">
+        <LoginButton />
+      </div>
+      <div className="flex justify-center items-center top-[100%] translate-y-[150%] dark:hidden ">
+        <Image src="/login_image.png" alt="login" width={500} height={500} />
+      </div>
+      <div className="flex justify-center items-center top-[100%] translate-y-[150%]">
+        <Image src="/login_image_white.png" alt="login" width={500} height={500} />
+      </div>
     </div>
   );
 }
--- src/lib/thirdWebClient.ts
+++ src/lib/thirdWebClient.ts
@@ -2,6 +2,6 @@ import { createThirdwebClient } from 'thirdweb';
 import { env } from '~/env';
 
 const clientId = env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
-const secretKey = env.THIRDWEB_SECRET_KEY;
+const secretKey = env.NEXT_PUBLIC_THIRDWEB_SECRET_KEY;
 
-export const twClient = createThirdwebClient(secretKey ? { secretKey } : { clientId }) 
+export const client = createThirdwebClient(secretKey ? { secretKey } : { clientId }) 
