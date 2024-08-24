323b21b384776e763f48a9c5d013431ce73dc753
Neha Sinha <nehasinha123987@gmail.com>
2024-08-25 02:56:05 +0530

    ,,,,

--- src/components/OnboardingForm.tsx
+++ src/components/OnboardingForm.tsx
@@ -85,7 +85,7 @@ function OnboardingForm({ walletAddress }: { walletAddress: string }) {
           </div>
 
           <Button type="submit">Submit</Button>
-          
+
         </form>
       </Form>
     </>
