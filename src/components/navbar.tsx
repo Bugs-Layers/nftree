b830405695410e6787d65aca5c05b6f41d95c032
Phynxxx <mail2syamantak@gmail.com>
2024-08-25 03:12:12 +0530

    navbar updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,19 +10,15 @@ export default function Navbar({
   sticky?: boolean
 }) {
   return (
-    <nav className={cn("h-[calc(8vh)] z-[10] w-full transition-all bg-[#f5f5f5] dark:bg-black", {
+    <nav className={cn("h-[calc(8vh)] z-[100] w-full transition-all bg-[#f5f5f5] dark:bg-black", {
       "sticky top-0": sticky,
     })}>
       <MaxWidthWrapper>
-        <div className="flex h-full w-full items-center justify-between bg-[#f5f5f5] dark:bg-black">
-          <span className="ml-5">
-          <div className="flex justify-center items-center dark:hidden absolute">
-            <Image src="/logo.png" alt="logo" width={40} height={40} />
-          </div>
-          <div className="flex justify-center items-center absolute">
-            <Image src="/logo_inverted.png" alt="logo" width={40} height={40} />
-          </div>
-          </span>
+        <div className="flex h-full items-center justify-between bg-[#f5f5f5] dark:bg-black">
+          <span><nav className="flex items-center gap-4 ">
+            <div className="dark:hidden">
+          <Image src="/logo.png" alt="logo" width={40} height={40} /></div>
+        </nav></span>
           <ThemeToggle />
         </div>
       </MaxWidthWrapper>
