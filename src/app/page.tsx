1ade6eaa6150330ee8dacc6fde6a8893a1319ee0
Neha Sinha <nehasinha123987@gmail.com>
2024-08-25 03:25:07 +0530

    onboarding header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ function page() {
   return (
     <div>
       <Profile avatar={"placeholder-user.jpg"} username={"John Doe"} description={"I am a software engineer who loves building cool stuff with code."} trees={100} carboncredits={290} />
-      <Onboarding walletAddress={"htf hfhfr"}/>
+      <Onboarding walletAddress={"htfhfhfr"}/>
     </div>
   )
 }
