cee17fb9ff0c2cf392cdc1b1e0ad11b6860e125a
Neha Sinha <nehasinha123987@gmail.com>
2024-08-24 22:34:24 +0530

    >>>

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,7 +4,7 @@ import Profile from "~/components/Profile"
 function page() {
   return (
     <div>
-      <Profile avatar={"placeholder-user.jpg"} username={"John Doe"} description={"I am a software engineer who loves building cool stuff with code."} trees={100} carboncredits={200}/>
+      <Profile avatar={"placeholder-user.jpg"} username={"John Doe"} description={"I am a software engineer who loves building cool stuff with code."} trees={100} carboncredits={290}/>
     </div>
   )
 }
